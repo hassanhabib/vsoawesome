52d146851030427fae49276d12d24065f3c71ec8
Hassan Rezk Habib <hassanhabib@live.com>
2019-11-05 01:39:29 +0000

    vso init

---
+++ Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace workspace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Hassan";
+            Console.WriteLine($"Hello, {name}");
+        }
+    }
+}
